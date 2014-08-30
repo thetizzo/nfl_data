44272018f5733db8fca098480aa419f4c0235cc8
thetizzo <j.m.taylor1@gmail.com>
2014-08-30 02:59:59 -0600

    Adding tests and some other stuff.

--- lib/nfl_data/player.rb
+++ lib/nfl_data/player.rb
@@ -1,37 +1,19 @@
 require 'nokogiri'
 require 'open-uri'
+require 'json'
 
 module NflData
   class Player
     attr_accessor :name, :position, :team, :status, :nfl_player_id
 
-    def initialize
-    end
-
-    def player_search_page
-      'http://www.nfl.com/players/search'
-    end
-
-    def positions
-      ['quarterback', 'runningback', 'widereceiver', 'tightend']
-    end
-
-    def get_by_position(position)
-      url_options = "?category=position&playerType=current&conference=ALL&filter=#{position}"
-      doc = Nokogiri::HTML(open(player_search_page + url_options))
-
-      player_rows = doc.search('table#result tbody tr')
-
-      player_rows.each do |row|
-        player_data = row.search('td')
-
-        @position = player_data[0].content.strip
-        @name = player_data[2].content.strip
-        @status = player_data[3].content.strip
-        @team = player_data[12].content.strip
-      end
-
-      self
+    def to_json
+      JSON.generate({
+        name: name,
+        position: position,
+        team: team,
+        status: status,
+        nfl_player_id: nfl_player_id
+      })
     end
   end
 end
---
+++ lib/nfl_data/player_search.rb
@@ -0,0 +1,30 @@
+module NflData
+  class PlayerSearch
+
+    def player_search_page
+      'http://www.nfl.com/players/search'
+    end
+
+    def positions
+      ['quarterback', 'runningback', 'widereceiver', 'tightend']
+    end
+
+    def get_by_position(position)
+      url_options = "?category=position&playerType=current&conference=ALL&filter=#{position}"
+      doc = Nokogiri::HTML(open(player_search_page + url_options))
+
+      player_rows = doc.search('table#result tbody tr')
+
+      player_rows.each do |row|
+        player_data = row.search('td')
+
+        @position = player_data[0].content.strip
+        @name = player_data[2].content.strip
+        @status = player_data[3].content.strip
+        @team = player_data[12].content.strip
+      end
+
+      self
+    end
+  end
+end
