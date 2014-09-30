0b31b367eeece201ab97fd92446eb51feb67cadb
thetizzo <j.m.taylor1@gmail.com>
2014-09-30 14:50:31 -0600

    Add methods to get runningbacks, wide receivers, and tight ends.

--- lib/nfl_data/api/player.rb
+++ lib/nfl_data/api/player.rb
@@ -12,6 +12,20 @@ module NflData
           JSON.generate({quarterbacks: players})
         end
 
+        def get_runningbacks
+          players = PlayerParser.new.get_by_position('runningback').map { |player| player.to_json }
+          JSON.generate({runningbacks: players})
+        end
+
+        def get_wide_receivers
+          players = PlayerParser.new.get_by_position('widereceiver').map { |player| player.to_json }
+          JSON.generate({wide_receivers: players})
+        end
+
+        def get_tight_ends
+          players = PlayerParser.new.get_by_position('tightend').map { |player| player.to_json }
+          JSON.generate({tight_ends: players})
+        end
       end
     end
   end
