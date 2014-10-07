c0bf7e058e85715fed7572379a3f2c674069ca92
thetizzo <j.m.taylor1@gmail.com>
2014-10-06 21:41:23 -0600

    Adding VCR tests for PlayerParser and some tests for API::Player

--- test/nfl_data/parsers/player_parser_test.rb
+++ test/nfl_data/parsers/player_parser_test.rb
@@ -12,4 +12,58 @@ describe PlayerParser do
   it 'should know the correct base url' do
     @parser.base_url.must_equal "http://www.nfl.com/players/search?category=position&conferenceAbbr=null&playerType=current&conference=ALL&filter="
   end
+
+  describe 'get_by_position' do
+    it 'should get quarterbacks' do
+      VCR.use_cassette('quarterbacks') do
+        response = @parser.get_by_position(:quarterbacks)
+
+        response.keys.must_include :quarterbacks
+        response[:quarterbacks].count.must_equal 125
+      end
+    end
+
+    it 'should get runningbacks' do
+      VCR.use_cassette('runningbacks') do
+        response = @parser.get_by_position(:runningbacks)
+
+        response.keys.must_include :runningbacks
+        response[:runningbacks].count.must_equal 259
+      end
+    end
+
+    it 'should get wide receivers' do
+      VCR.use_cassette('wide_receivers') do
+        response = @parser.get_by_position(:wide_receivers)
+
+        response.keys.must_include :wide_receivers
+        response[:wide_receivers].count.must_equal 383
+      end
+    end
+
+    it 'should get tight ends' do
+      VCR.use_cassette('tight_ends') do
+        response = @parser.get_by_position(:tight_ends)
+
+        response.keys.must_include :tight_ends
+        response[:tight_ends].count.must_equal 192
+      end
+    end
+
+    it 'should get all the players' do
+      VCR.use_cassette('all_players') do
+        response = @parser.get_by_position(:all)
+
+        {
+          quarterbacks: 125,
+          runningbacks: 259,
+          wide_receivers: 383,
+          tight_ends: 192
+        }.each do |position, player_count|
+          response.keys.must_include position
+          response[position].count.must_equal player_count
+        end
+      end
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,11 @@ require 'nfl_data'
 
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'vcr'
 
 include NflData
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/fixtures/cassettes'
+  c.hook_into :webmock
+end
