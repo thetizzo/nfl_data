b83ce0eef144615fe93aa4e54131867365e1e1d2
thetizzo <j.m.taylor1@gmail.com>
2015-11-24 00:56:35 -0700

    Refactor models with more than a few attributes so that they can be initialized by passing a hash and make their accessor and to_hash code a little more dynamic.  This is so that they are easier to add attributes to in the future because you will only have to add them in the ATTRIBUTES list.

--- lib/nfl_data/models/player.rb
+++ lib/nfl_data/models/player.rb
@@ -1,19 +1,20 @@
 module NflData
   class Player
-    attr_accessor :first_name, :last_name, :full_name, :position, :number, :status, :team, :nfl_player_id
+    ATTRIBUTES = [
+      :first_name, :last_name, :full_name, :position, :number,
+      :status, :team, :nfl_player_id
+    ]
 
-    def to_hash
-      {
-        first_name: first_name,
-        last_name: last_name,
-        full_name: full_name,
-        position: position,
-        number: number,
-        status: status,
-        team: team,
-        nfl_player_id: nfl_player_id
-      }
+    attr_accessor(*ATTRIBUTES)
+
+    def initialize(attributes = {})
+      attributes.each { |attr, value| send("#{attr}=", value) }
     end
 
+    def to_hash
+      attributes_hash = {}
+      ATTRIBUTES.each { |attr| attributes_hash.merge!(attr => send(attr)) }
+      attributes_hash
+    end
   end
 end
