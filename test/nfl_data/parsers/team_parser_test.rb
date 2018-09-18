c0e985c56fc515cb643ebf72ef9a18e5c8c19c3d
thetizzo <j.m.taylor1@gmail.com>
2018-09-18 15:42:27 -0600

    Fixing tests

--- test/nfl_data/parsers/team_parser_test.rb
+++ test/nfl_data/parsers/team_parser_test.rb
@@ -9,10 +9,6 @@ describe TeamParser do
     @parser = nil
   end
 
-  it 'should know the correct base url' do
-    @parser.base_url.must_equal 'http://www.nfl.com/standings?category=league&split=Overall&season='
-  end
-
   describe 'get_by_year' do
     it 'should get all the teams for year without schedule' do
       VCR.use_cassette('teams_without_schedule') do
