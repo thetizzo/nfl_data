8e78ed19070a7806b99d3fc7383f371cbfe59579
thetizzo <j.m.taylor1@gmail.com>
2020-08-15 00:32:18 -0600

    Removed dry-configurable
    This doesn't seem to work quite the way I expected and I don't
    really have time to figure it out so I'm removing it for now.

--- lib/nfl_data.rb
+++ lib/nfl_data.rb
@@ -3,17 +3,9 @@ require "open-uri"
 require "json"
 require "typhoeus"
 require "base64"
-require "dry/configurable"
 require "zeitwerk"
 
 Zeitwerk::Loader.for_gem.setup
 
 module NflData
-  extend Dry::Configurable
-
-  setting :my_sports_feeds_api do
-    setting :key, ENV.fetch("MYSPORTSFEEDS_API_KEY")
-    setting :host, ENV.fetch("MYSPORTSFEEDS_API_HOST")
-    setting :version, ENV.fetch("MYSPORTSFEEDS_API_VERSION")
-  end
 end
