00e1230481c6bb855edc8e19cd917f757f5c0873
thetizzo <j.m.taylor1@gmail.com>
2020-09-01 00:28:23 -0600

    Added a reader for feed on schedule

--- lib/nfl_data/api/schedule.rb
+++ lib/nfl_data/api/schedule.rb
@@ -3,7 +3,7 @@
 module NflData
   module Api
     class Schedule
-      attr_reader :parser
+      attr_reader :parser, :feed
 
       def initialize(parser: Parsers::ScheduleParser.new, feed: MySportsFeeds::SeasonalGamesFeed.new)
         @parser = parser
@@ -11,7 +11,7 @@ module NflData
       end
 
       def season(year:)
-        schedule_data = @feed.feed(season_start_year: year)
+        schedule_data = feed.feed(season_start_year: year)
         parser.parse(schedule_data: schedule_data).to_h.to_json
       end
     end
